e6e566a1e7e56fa0716512bf0c975124b59bcb6c
aswin-asokan <aswinachoos2248@gmail.com>
2023-01-24 22:33:13 +0530

    search bar added

--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart';
 
 class Map extends StatefulWidget {
   const Map({super.key});
@@ -10,30 +11,60 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(9.999546, 76.307841),
+    zoom: 14.4746,
+  );
+
+  late GoogleMapController mapController;
+  late String search;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FlutterMap(
-        options: MapOptions(
-            center: LatLng(9.9816, 76.2999), zoom: 12, maxZoom: 18.4),
-        nonRotatedChildren: [
-          AttributionWidget.defaultWidget(
-            source: 'OpenStreetMap contributors',
-            onSourceTapped: null,
-          ),
-        ],
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-        ],
-      ),
+      body: Stack(children: [
+        GoogleMap(
+          onMapCreated: onMapCreated,
+          zoomControlsEnabled: false,
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+        ),
+        Positioned(
+            top: 30.0,
+            right: 15.0,
+            left: 15.0,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Location",
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
+                    suffixIcon: IconButton(
+                      onPressed: () => {},
+                      icon: Icon(Icons.search),
+                      iconSize: 30.0,
+                    )),
+                onChanged: (value) {
+                  search = value;
+                },
+              ),
+            ))
+      ]),
       appBar: AppBar(
         title: Text('Map'),
         backgroundColor: Color.fromRGBO(28, 103, 88, 1),
       ),
     );
     ;
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
